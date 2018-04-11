440bc1158f5566a332df6cecc6a237eb352f0c3d
hekiheki <hekibun@gmail.com>
2018-04-11 12:52:52 +0800

    up

--- README.md
+++ README.md
@@ -2,14 +2,20 @@
 
 1. react+webpack的一个小型结构参考了，参考了<https://www.jianshu.com/p/db6113c94dbc> 的配置，主要用于写react组件。
 2. 加入了sass-loader,file-loader。
-3. 没有加入react-router和redux等。
+3. <del>没有加入react-router和redux等</del>加入了react-router。
+
+
+##目录结构
+
+![](fileTree.png)
+
 
 ## 使用
 
 ```
 $ git clone https://github.com/hekiheki/start-react-app.git
 $ npm install
-$ npm run start
+$ npm start
 
 //build
 $ npm run build
--- webpack.config.js
+++ webpack.config.js
@@ -1,16 +1,24 @@
-const HtmlWebpackPlugin = require('html-webpack-plugin');
-const ExtractTextPlugin = require('extract-text-webpack-plugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin'); //html模板
+const ExtractTextPlugin = require('extract-text-webpack-plugin'); //单独打包css
+const UglifyJSPlugin = require('uglifyjs-webpack-plugin'); //压缩
+const CleanWebpackPlugin = require('clean-webpack-plugin'); //清理
 const webpack = require('webpack'); //访问内置的插件
 const path = require('path');
 
 const config = {
 	// 入口文件，entry
-	entry: './src/index.js',
+	entry: {
+		app: [
+			'react-hot-loader/patch',
+	        path.join(__dirname, './src/index.js')
+		],
+		vendor: ['react', 'react-router-dom', 'react-dom']
+	},
 	// 出口
 	output: {
 		path: path.resolve(__dirname, './build'),
-		filename: 'bundle.js',
-		publicPath: "./",
+		filename: '[name].[chunkhash].js',
+        chunkFilename: '[name].[chunkhash].js',
 	},
 	devtool: "sourcemap",
 	// loader,处理非javascript文件
@@ -19,21 +27,24 @@ const config = {
 			{ 
 				test: /\.js$/,
 				use: ['babel-loader?cacheDirectory=true'],
-				include: path.join(__dirname, 'src')
+				include: path.join(__dirname, 'src'),
+				exclude: /node_modules/
 			},
 			{
 				test: /\.s?css$/,
                 use: ExtractTextPlugin.extract({
 					fallback:"style-loader",
 					use: ['css-loader','sass-loader'],
-					publicPath: "/build"  
 				  })
 			},
 			{
-		        test: /\.(png|jpg|gif)$/,
+		        test: /\.(png|jpg|gif|svg)$/,
 		        use: [
 		          {
-		            loader: 'file-loader'
+		            loader: 'url-loader',
+			        options: {
+			            limit: 8192
+			        }
 		          }
 		        ]
 		      }
@@ -42,17 +53,38 @@ const config = {
 	plugins: [
 		new HtmlWebpackPlugin({template: './public/index.html'}),
 		new ExtractTextPlugin({
-			filename: "styles.css",
+			filename: '[name].[contenthash:5].css',
 			disable: false,
 			allChunks: true
-  		})
+  		}),
+        new UglifyJSPlugin(),
+        new webpack.DefinePlugin({
+          'process.env': {
+              'NODE_ENV': JSON.stringify('production')
+           }
+       }),
+        new webpack.optimize.CommonsChunkPlugin({
+            name: 'vendor'
+        }),
+       	new webpack.HashedModuleIdsPlugin(),
+       	new webpack.optimize.CommonsChunkPlugin({
+		    name: 'runtime'
+		}),
+		new CleanWebpackPlugin(['build'])
 	],
 	resolve: {
         alias: {
-            components: path.join(__dirname, 'src/components'),
+            pages: path.join(__dirname, 'src/pages'),
+            component: path.join(__dirname, 'src/component'),
+            router: path.join(__dirname, 'src/router')
         }
 	},
-	devtool: 'inline-source-map'
+	devtool: 'inline-source-map',
+	devServer: {
+        port: 8080,
+        contentBase: path.join(__dirname, './build'),
+        historyApiFallback: true,
+    }
 };
 
 module.exports = config;
